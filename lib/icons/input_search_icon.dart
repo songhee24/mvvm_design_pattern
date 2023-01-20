51cd49ca7922b8fa3ce317ad4d8db75fe0a78b1e
songhee24 <akaratel61@gmail.com>
2023-01-21 04:16:31 +0600

    custom search works

--- lib/icons/input_search_icon.dart
+++ lib/icons/input_search_icon.dart
@@ -22,9 +22,7 @@ import 'package:flutter/widgets.dart';
 class InputSearchIcon {
   InputSearchIcon._();
 
-  static const _kFontFam = 'MyFlutterApp';
-  static const String? _kFontPkg = null;
+  static const _kFontFam = 'InputSearchIcon';
 
-  static const IconData search =
-      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData search = IconData(0xe800, fontFamily: _kFontFam);
 }

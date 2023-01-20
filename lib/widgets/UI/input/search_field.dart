00e7421325a9f01f6320f851474fce2b59bda6a6
songhee24 <akaratel61@gmail.com>
2023-01-21 03:41:08 +0600

    setted my custom icon

---
+++ lib/widgets/UI/input/search_field.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+///  Created by mac on 21/1/23.
+class SearchField extends StatefulWidget {
+  const SearchField({required Key key}) : super(key: key);
+
+  @override
+  _SearchFieldState createState() => _SearchFieldState();
+}
+
+class _SearchFieldState extends State<SearchField> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

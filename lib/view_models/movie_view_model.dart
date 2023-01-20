ea6dd88e60dc52836945720c441256a08fa18211
songhee24 <akaratel61@gmail.com>
2023-01-21 01:16:38 +0600

    created model of movie which will response of view model

---
+++ lib/view_models/movie_view_model.dart
@@ -0,0 +1,15 @@
+import '../models/movie.dart';
+
+class MovieViewModel {
+  final Movie movie;
+
+  MovieViewModel({required this.movie});
+
+  String get title {
+    return movie.title;
+  }
+
+  String get poster {
+    return movie.poster;
+  }
+}
